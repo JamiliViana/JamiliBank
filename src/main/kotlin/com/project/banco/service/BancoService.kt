367f7de627899d982d704f63e27831e604266e2c
Jamili Viana <jamilimartucheliviana@gmail.com>
2023-01-16 14:15:54 -0300

    fix: delete imports

--- src/main/kotlin/com/project/banco/service/BancoService.kt
+++ src/main/kotlin/com/project/banco/service/BancoService.kt
@@ -1,9 +1,6 @@
 package com.project.banco.service
 
 import com.project.banco.domains.Conta
-import com.project.banco.persistence.jpa.entity.ContaEntity
-import org.springframework.stereotype.Service
-import java.util.*
 
 
 interface BancoService {
